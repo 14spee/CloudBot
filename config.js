cd06c3e7a138be3407c6688095f7da16f36d77f1
14spee <98434772+14spee@users.noreply.github.com>
2022-06-08 08:31:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -11,13 +11,13 @@ global.sgh = process.env.LINK_GITHUB
 global.sgc = process.env.LINK_GROUP
 global.ownernumber = process.env.NOWNER
 global.owner = [
-  ['0'],
-  ['0'],
+  ['4915678366725'],
+  ['4915678366725'],
   [`${process.env.NOWNER}`, `${process.env.NAMAOWNER}`, true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [`${process.env.MODERATOR}`] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.prems = [4915678366725] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
